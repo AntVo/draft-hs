f30b9eef07b5dc901be3d8acb565fdc365e91f39
antoinevo <antoine.monarch@gmail.com>
2018-03-29 23:13:40 -0700

    refacotr

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var socket = require('socket.io');
 
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(4030);
 var io = socket(server);
 
 console.log("Socket server is running");
@@ -12,7 +12,7 @@ io.sockets.on('connection', newConnection);
 
 function newConnection(socket){
 	console.log('new connection' + socket.id);
-	socket.on('mouse', mouseMessage);
+	// socket.on('mouse', mouseMessage);
 	
 	function mouseMessage(data){
 		socket.broadcast.emit('mouse', data);
