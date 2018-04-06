e9bcf204e936d708e28ea1e4289a2f321d869cbb
antoinevo <antoine.monarch@gmail.com>
2018-04-06 09:16:11 -0700

    backend

--- backend/server.js
+++ backend/server.js
@@ -29,9 +29,24 @@ class Drafter {
   }
 }
 
+function runDraft(room){
+  for (i = 0; i < 3; i++){
+    runRound(room);
+  }
+}
+
+function runRound(room){
+  // Give everyone a pack
+
+   // while peoples packs are not empty,
+   // user picks a card and puts it in
+   // passes to next
+}
+
+
 // Mock Database 
 // A room should have { roomID, format, packs}
-const rooms = []; 
+const rooms = {};
 
 // This creates our socket using the instance of the server
 const io = socketIO(server)
@@ -45,18 +60,25 @@ lobbySocket.on('connection', socket => {
     console.log('user disconnected');
   })
 
-  socket.on('create room', (format, roomID) => {
-  	console.log('creating room: ' + roomID + ' format: ' + format);
-    const room = new Room(format, roomID)
-  	rooms.push(room);
+  socket.on('create room', (format) => {
+    const length = Object.keys(rooms).length
+  	console.log('creating room: ' + length + ' format: ' + format);
+    const room = new Room(format, length)
+  	rooms[length] = room;
   })
 
   socket.on('roomlist', () => {
     socket.emit('roomlist', rooms);
   })
 
-  socket.on('joinroom', (roomID) => {
-    console.log('joining ' + roomID);
+  socket.on('joinroom', (roomID, username) => {
+    console.log(username + ' is joining ' + roomID);
+
+    const drafter = new Drafter(1, username);
+    rooms[roomID].people.push(drafter);
+
+    const length = Object.keys(rooms[roomID].people).length;
+    console.log('number of players in room: ' + length);
     socket.join(roomID);
   })
 
